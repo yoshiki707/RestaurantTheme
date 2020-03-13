02aaa03a98ba4cc070de8d31a58dc48a79be9ee0
yoshiki707 <56143537+yoshiki707@users.noreply.github.com>
2020-03-13 23:30:13 +0900

    Add files via upload
    ファイルの追加

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 1500,
+    pauseOnHover: true,
+    slideToShow: 1,
+    fade: true,
+    swipe: true,
+    focusOnSelect: true,
+    dots: true,
+  });
+});
